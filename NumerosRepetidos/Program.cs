101d8c529ea16ac9bc7989b17737c3abe941978f
Amaarillo2410 <225398294+Amarillo2410@users.noreply.github.com.>
2026-03-21 23:14:44 -0500

    :white_check_mark: finalizacion ejercicio2

--- NumerosRepetidos/Program.cs
+++ NumerosRepetidos/Program.cs
@@ -1,1 +1,69 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class EjercicioDos
+{
+    static void Main()
+    {
+        List<int> Numeros = new List<int>();
+        int opciones = 0;
+        while (opciones != 4)
+        {
+            Console.WriteLine("--MENU--");
+            Console.WriteLine("1. Agregar numero a la lista");
+            Console.WriteLine("2. Mostrar numeros en la lista");
+            Console.WriteLine("3. eliiminar numeros de la lista");
+            Console.WriteLine("4. Salir");
+
+
+            Console.WriteLine("ingrese valosres a la lista");
+            if(!int.TryParse(Console.ReadLine(), out int enteros) )
+            {
+                Console.WriteLine("Opcion no valida");
+                continue;
+            }
+            switch(enteros){
+                case 1:
+                    Console.WriteLine("Ingrese un numero para agregar a la lista");
+                    if(int.TryParse(Console.ReadLine(), out int numeroAgregar))
+                    {
+                        Numeros.Add(numeroAgregar);
+                        Console.WriteLine("Se ha agregado " + numeroAgregar + " a la lista");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numero no valido");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Lista: ");
+                    foreach (int numero in Numeros)
+                    {
+                        Console.WriteLine(numero);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese un numero para eliminar de la lista");
+                    if(int.TryParse(Console.ReadLine(), out int numeroEliminar))
+                    {
+                        if(Numeros.Remove(numeroEliminar))
+                        {
+                            Console.WriteLine("Numero eliminado de la lista");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Numero no encontrado en la lista");
+                        }
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("Saliendo del programa...");
+                    return;
+            default:
+                Console.WriteLine("Opcion no valida");
+                break;
+            }
+
+        }
+    }
+}
