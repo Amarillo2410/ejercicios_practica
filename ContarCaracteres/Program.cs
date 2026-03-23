8c79631889da2677b5ee64c73c7055111ce5d9a2
Amaarillo2410 <225398294+Amarillo2410@users.noreply.github.com.>
2026-03-22 21:48:11 -0500

    :white_check_mark:Ejercicio 4 finalizado

---
+++ ContarCaracteres/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+
+class Program
+{
+    // Clase para guardar resultados
+    class Frecuencia
+    {
+        public char Car { get; set; }
+        public int Veces { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa un texto:");
+        string texto = Console.ReadLine() ?? String.Empty;
+
+        // 🔹 Evita error si el usuario no escribe nada
+        if (string.IsNullOrEmpty(texto))
+        {
+            Console.WriteLine("Texto vacío");
+            return;
+        }
+
+        // 🔹 Minúsculas
+        string normalizado = texto.ToLower();
+
+        // 🔹 Quitar tildes
+        string textoNormalizado = normalizado.Normalize(NormalizationForm.FormD);
+        string sinTildes = "";
+
+        foreach (char c in textoNormalizado)
+        {
+            if (Char.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+            {
+                sinTildes += c;
+            }
+        }
+
+        // 🔹 Diccionario para contar
+        Dictionary<char, int> contador = new Dictionary<char, int>();
+
+        foreach (char c in sinTildes)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                if (contador.ContainsKey(c))
+                    contador[c]++;
+                else
+                    contador[c] = 1;
+            }
+        }
+
+        // 🔹 Convertir y ordenar
+        var resultado = contador
+            .Select(x => new Frecuencia { Car = x.Key, Veces = x.Value })
+            .OrderBy(x => x.Car)
+            .ToList();
+
+        // 🔹 Mostrar
+        Console.WriteLine("\nFrecuencia:");
+
+        foreach (var item in resultado)
+        {
+            Console.WriteLine($"Car: {item.Car} - Veces: {item.Veces}");
+        }
+    }
+}
---
+++ GeneradorContraseña/README.md
@@ -0,0 +1,15 @@
+# 3. Generador de contraseñas seguras
+
+En este reto debes crear un programa en **C# para consola** que genere contraseñas aleatorias.
+
+Las contraseñas generadas deben cumplir las siguientes condiciones:
+
+- Tener una longitud **entre 15 y 87 caracteres**.
+- Contener **al menos una letra mayúscula**.
+- Contener **al menos una letra minúscula**.
+- Contener **al menos un dígito**.
+- Contener **al menos un carácter especial**.
+
+Los caracteres especiales permitidos son únicamente los siguientes:
+
+! @ # $ % & * ? + = - /
